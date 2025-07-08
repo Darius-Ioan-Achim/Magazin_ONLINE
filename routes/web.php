290f85cc6e7064f0bd3d39c26bd2edddc369b797
Achim-Darius-Ioan <dariusioan.achim@ulbsibiu.ro>
2025-07-08 14:01:54 +0300

    create HomePage

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
+Route::get('/homepage', function () {
+    return Inertia::render('HomePage', [
+        'breadcrumbs' => [
+            ['title' => 'HomePage', 'href' => '/homepage'],
+        ],
+    ]);
+})->name('homepage');
+
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
